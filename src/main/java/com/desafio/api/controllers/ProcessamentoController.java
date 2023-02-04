10a8922afc02b49cff47da8c4cd378c15b094c20
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-04 17:46:08 -0300

    feat: Controlador e Service do Processamento criados * Controlador de Processamento dos pagamentos criado * Serviço para lidar com regras de negócio do Processamento de pagamentos criado

--- src/main/java/com/desafio/api/controllers/ProcessamentoController.java
+++ src/main/java/com/desafio/api/controllers/ProcessamentoController.java
@@ -1,13 +1,19 @@
 package com.desafio.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafio.api.config.exception.ControllersException;
+import com.desafio.api.config.response.ResponseHandler;
 import com.desafio.api.model.*;
 import com.desafio.api.services.*;
 
@@ -20,8 +26,24 @@ public class ProcessamentoController {
     ProcessamentoService processamentoService;
 
     @PutMapping
-    public void atualizarProcessamentoDePagamento(@RequestParam(value = "id") Long paramId,
-            @RequestParam(value = "status") String status) {
+    public ResponseEntity<Object> atualizarProcessamentoDePagamento(@RequestParam(value = "id") Long pagamentoId,
+            @RequestParam(value = "status") String status) throws ControllersException {
+        try {
+            Pagamento pagamento = processamentoService.atualizar(pagamentoId, status);
+
+            Map<String, String> response = new HashMap<>();
+
+            if (pagamento == null) {
+                response.put("error", "Id do pagamento não existe!");
+
+                return new ResponseEntity(response, HttpStatus.NOT_FOUND);
+            }
+
+            return ResponseHandler.responseBuilder("Pagamento atualizado com sucesso!", HttpStatus.OK, pagamento);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
 
     }
 }
--- src/main/java/com/desafio/api/services/ProcessamentoService.java
+++ src/main/java/com/desafio/api/services/ProcessamentoService.java
@@ -1,22 +1,57 @@
 package com.desafio.api.services;
 
-import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import com.desafio.api.Repository.*;
-import com.desafio.api.config.exception.ApiErrorMessage;
-import com.desafio.api.dto.*;
+
+import com.desafio.api.config.exception.ControllersException;
 import com.desafio.api.model.Pagamento;
-import com.desafio.api.utils.MetodoPagamento;
-import com.desafio.api.*;
-import java.util.*;
+import com.desafio.api.repository.*;
 
 @Service
 public class ProcessamentoService {
 
     @Autowired
     PagamentoRepository pagamentoRepository;
+
+    public Pagamento atualizar(Long pagamentoId, String status) throws ControllersException {
+        Pagamento pagamento = pagamentoRepository.findById(pagamentoId).orElse(null);
+
+        boolean statusDiferentePendente = !status.equals("pendente");
+        boolean statusDiferenteSucesso = !status.equals("sucesso");
+        boolean statusDiferenteFalha = !status.equals("falha");
+
+        if (pagamento == null) {
+            return null;
+        }
+
+        String pagamentoStatus = pagamento.getStatus();
+
+        if (statusDiferenteSucesso && statusDiferenteFalha && statusDiferentePendente) {
+            throw new ControllersException(HttpStatus.BAD_REQUEST, "processamento não aceita este tipo de valor");
+
+        }
+
+        if (pagamentoStatus.equals("pendente") && statusDiferentePendente) {
+            pagamento.setStatus(status);
+
+            pagamentoRepository.save(pagamento);
+
+        } else if (pagamentoStatus.equals("sucesso")) {
+            throw new ControllersException(HttpStatus.BAD_REQUEST, "Pagamento já aprovado não pode ser alterado!");
+
+        } else if (pagamentoStatus.equals("falha")) {
+
+            if (statusDiferentePendente) {
+                throw new ControllersException(HttpStatus.BAD_REQUEST,
+                        "Não pode ser alterado para outro valor além de pendente");
+
+            }
+            pagamento.setStatus(status);
+            pagamentoRepository.save(pagamento);
+        }
+
+        return pagamento;
+
+    }
 }
