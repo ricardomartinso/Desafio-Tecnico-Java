b6025f390c9f8717a1974ec75214ab048dbb41b5
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-04 17:48:49 -0300

    feat: adicionado features de deletar e filtrar pagamentos e handle de Erros e Response

--- src/main/java/com/desafio/api/controllers/PagamentoController.java
+++ src/main/java/com/desafio/api/controllers/PagamentoController.java
@@ -1,21 +1,27 @@
 package com.desafio.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafio.api.config.exception.ControllersException;
+import com.desafio.api.config.response.ResponseHandler;
 import com.desafio.api.dto.PagamentoDTO;
 import com.desafio.api.services.*;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.desafio.api.model.*;
+
 import java.util.*;
 
 @RestController
@@ -33,11 +39,60 @@ public class PagamentoController {
 
     }
 
-    @PostMapping()
-    public Object criarPagamento(@RequestBody @Valid PagamentoDTO req) {
+    @GetMapping("/codigo-debito/{codigoDebito}")
+    public List<Pagamento> listarPagamentosPorCodigoDebito(@PathVariable int codigoDebito) {
 
-        return pagamentoService.salvar(req);
+        return pagamentoService.listarPorCodigoDebito(codigoDebito);
 
     }
 
+    @GetMapping("/cpf-cnpj/{cpfCnpj}")
+    public List<Pagamento> listarPagamentosPorCpfCnpj(@PathVariable String cpfCnpj) {
+
+        return pagamentoService.listarPorCpfCnpj(cpfCnpj);
+
+    }
+
+    @GetMapping("/status/{status}")
+    public List<Pagamento> listarPagamentosPorStatus(@PathVariable String status) {
+
+        return pagamentoService.listarPorStatus(status);
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> criarPagamento(@RequestBody @Valid PagamentoDTO req) throws ControllersException {
+
+        try {
+            Pagamento pagamento = pagamentoService.salvar(req);
+
+            return ResponseHandler.responseBuilder("Pagamento criado com sucesso!", HttpStatus.CREATED, pagamento);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletarPagamento(@PathVariable Long id) {
+
+        try {
+            Pagamento pagamento = pagamentoService.deletarPagamentoPorId(id);
+
+            Map<String, String> response = new HashMap<>();
+
+            if (pagamento == null) {
+                response.put("error", "Id do pagamento não existe!");
+
+                return new ResponseEntity(response, HttpStatus.NOT_FOUND);
+            }
+
+            return ResponseHandler.responseBuilder("Pagamento deletado com sucesso!", HttpStatus.OK, pagamento);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
 }
--- src/main/java/com/desafio/api/services/PagamentoService.java
+++ src/main/java/com/desafio/api/services/PagamentoService.java
@@ -1,17 +1,15 @@
 package com.desafio.api.services;
 
-import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import com.desafio.api.Repository.*;
-import com.desafio.api.config.exception.ApiErrorMessage;
+
+import com.desafio.api.config.exception.ControllersException;
 import com.desafio.api.dto.*;
 import com.desafio.api.model.Pagamento;
+import com.desafio.api.repository.*;
 import com.desafio.api.utils.MetodoPagamento;
-import com.desafio.api.*;
+
 import java.util.*;
 
 @Service
@@ -20,7 +18,7 @@ public class PagamentoService {
     @Autowired
     private PagamentoRepository pagamentoRepository;
 
-    public Object salvar(PagamentoDTO req) {
+    public Pagamento salvar(PagamentoDTO req) throws ControllersException {
 
         MetodoPagamento metodoPagamento = req.metodoPagamento();
         String cpf = null;
@@ -31,26 +29,25 @@ public class PagamentoService {
         } else if (req.cpfCnpjPagador().length() == 14) {
             cnpj = req.cpfCnpjPagador();
         } else {
-
-            return new ApiErrorMessage(HttpStatus.BAD_REQUEST, "CPF/CNPJ inválido");
+            throw new ControllersException(HttpStatus.BAD_REQUEST, "CPF/CNPJ inválidos!");
         }
 
         if ((metodoPagamento == MetodoPagamento.BOLETO || metodoPagamento == MetodoPagamento.PIX)) {
             if (req.numeroCartao().isEmpty()) {
-                Pagamento pag = pagamentoRepository.save(new Pagamento(req, cpf, cnpj));
-                return pag;
+                return pagamentoRepository.save(new Pagamento(req, cpf, cnpj));
 
             }
-            return null;
+
+            throw new ControllersException(HttpStatus.BAD_REQUEST,
+                    "Número de cartão inválido com esse meio de pagamento");
 
         } else {
-
             if (req.numeroCartao().isEmpty()) {
-                return null;
-            }
-            Pagamento pag = pagamentoRepository.save(new Pagamento(req, cpf, cnpj));
+                throw new ControllersException(HttpStatus.BAD_REQUEST,
+                        "Número de cartão faltando");
 
-            return pag;
+            }
+            return pagamentoRepository.save(new Pagamento(req, cpf, cnpj));
 
         }
 
@@ -58,5 +55,38 @@ public class PagamentoService {
 
     public List<Pagamento> listarTodos() {
         return pagamentoRepository.findAll();
+    }
+
+    public List<Pagamento> listarPorCodigoDebito(int codigoDebito) {
+        return pagamentoRepository.findByCodigoDebito(codigoDebito);
+    }
+
+    public List<Pagamento> listarPorCpfCnpj(String cpfCnpj) {
+
+        if (cpfCnpj.length() == 11) {
+            return pagamentoRepository.findByCpf(cpfCnpj);
+        } else if (cpfCnpj.length() == 14) {
+            return pagamentoRepository.findByCnpj(cpfCnpj);
+        }
+
+        return null;
+
+    }
+
+    public List<Pagamento> listarPorStatus(String status) {
+        return pagamentoRepository.findByStatus(status);
+    }
+
+    public Pagamento deletarPagamentoPorId(Long id) {
+
+        Pagamento pagamento = pagamentoRepository.findById(id).orElse(null);
+
+        if (pagamento == null) {
+            return null;
+        }
+
+        pagamentoRepository.deleteById(id);
+
+        return pagamento;
     }
 }
