3d4c03668c0ea709b024d82331739608c36cd52f
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-02-04 17:43:41 -0300

    refactor: mudanças nas propriedades da tabela e colunas * Criado nome para tabela pagamento * Adicionado updatable false para campos que não podem ser alterados

--- src/main/java/com/desafio/api/model/Pagamento.java
+++ src/main/java/com/desafio/api/model/Pagamento.java
@@ -1,59 +1,55 @@
 package com.desafio.api.model;
 
 import com.desafio.api.dto.PagamentoDTO;
-import com.desafio.api.utils.MetodoPagamento;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Optional;
-
-import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name = "pagamentos")
 public class Pagamento {
     public Pagamento(PagamentoDTO pagamentoDTO, String cpf, String cnpj) {
 
         this.codigoDebito = pagamentoDTO.codigoDebito();
         this.cpf = cpf;
         this.cnpj = cnpj;
-        this.metodoPagamento = pagamentoDTO.metodoPagamento();
+        this.metodoPagamento = pagamentoDTO.metodoPagamento().toString();
         this.numeroCartao = pagamentoDTO.numeroCartao().orElse(null);
         this.valorPagamento = pagamentoDTO.valorPagamento();
-        this.processamento = "Pendente";
+        this.status = "pendente";
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column
+    @Column(updatable = false)
     int codigoDebito;
 
-    @Column(length = 11, nullable = true)
+    @Column(length = 11, nullable = true, updatable = false)
     String cpf;
 
-    @Column(length = 14, nullable = true)
+    @Column(length = 14, nullable = true, updatable = false)
     String cnpj;
 
-    @Column(length = 50, nullable = false)
-    MetodoPagamento metodoPagamento;
+    @Column(length = 50, nullable = false, updatable = false)
+    String metodoPagamento;
 
-    @Column(length = 50, nullable = true)
+    @Column(length = 50, nullable = true, updatable = false)
     String numeroCartao;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 50, nullable = false, updatable = false)
     double valorPagamento;
 
-    @Column
-    String processamento;
+    @Column(length = 50, nullable = false)
+    String status;
 
 }
