290f368055e6b3a629f27d33be26a4c7c6232e7f
Ricardo Martins <ricardomartinso.dev@gmail.com>
2023-03-21 17:28:14 -0300

    refactor: controlador de processamentos recebe DTO e Json body

--- src/main/java/com/desafio/api/controllers/ProcessamentoController.java
+++ src/main/java/com/desafio/api/controllers/ProcessamentoController.java
@@ -8,14 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.api.config.exception.ApiExceptionMessage;
 import com.desafio.api.config.response.ResponseHandler;
+import com.desafio.api.dto.ProcessamentoDTO;
 import com.desafio.api.model.*;
 import com.desafio.api.services.*;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/process")
@@ -26,10 +30,12 @@ public class ProcessamentoController {
     ProcessamentoService processamentoService;
 
     @PutMapping
-    public ResponseEntity<Object> atualizarProcessamentoDePagamento(@RequestParam(value = "id") Long pagamentoId,
-            @RequestParam(value = "status") String status) {
+    public ResponseEntity<Object> atualizarProcessamentoDePagamento(
+            @RequestBody @Valid ProcessamentoDTO processamentoDto) {
         try {
-            Pagamento pagamento = processamentoService.atualizar(pagamentoId, status);
+
+            Pagamento pagamento = processamentoService.atualizar(processamentoDto.id(),
+                    processamentoDto.statusPagamento());
 
             Map<String, String> response = new HashMap<>();
 
